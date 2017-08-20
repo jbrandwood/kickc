ffce3298efa111956df517b284ccd10866f41549
jespergravgaard <jesper@balmangravgaard.dk>
2017-08-20 21:12:40 +0200

    Moved bresenham into main()

--- src/main/java/dk/camelot64/kickc/test/ref/bresenham.asm
+++ src/main/java/dk/camelot64/kickc/test/ref/bresenham.asm
@@ -1,40 +1,44 @@
-  lda #$0
-  sta $5
-  ldx #$c
-  lda #$0
-  sta $4
-  lda #<$400
-  sta $2
-  lda #>$400
-  sta $2+$1
-b1:
-  ldy #$0
-  lda #$51
-  sta ($2),y
-  inc $4
-  inc $2
-  bne !+
-  inc $2+$1
-!:
-  txa
-  clc
-  adc #$18
-  tax
-  cpx #$27
-  bcc b2
-  inc $5
-  lda $2
-  clc
-  adc #$28
-  sta $2
-  bcc !+
-  inc $2+$1
-!:
-  txa
-  sec
-  sbc #$27
-  tax
-b2:
-  lda $4
-  cmp #$28
-  bcc b1
+  jsr main
+main: {
+    lda #$0
+    sta $5
+    ldx #$c
+    lda #$0
+    sta $4
+    lda #<$400
+    sta $2
+    lda #>$400
+    sta $2+$1
+  b1:
+    ldy #$0
+    lda #$51
+    sta ($2),y
+    inc $4
+    inc $2
+    bne !+
+    inc $2+$1
+  !:
+    txa
+    clc
+    adc #$18
+    tax
+    cpx #$27
+    bcc b2
+    inc $5
+    lda $2
+    clc
+    adc #$28
+    sta $2
+    bcc !+
+    inc $2+$1
+  !:
+    txa
+    sec
+    sbc #$27
+    tax
+  b2:
+    lda $4
+    cmp #$28
+    bcc b1
+    rts
+}

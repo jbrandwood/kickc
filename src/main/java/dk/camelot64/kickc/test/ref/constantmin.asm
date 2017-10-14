525ac454cca4745c255adc4301f349c90d7a24b2
jespergravgaard <jesper@balmangravgaard.dk>
2017-10-14 22:37:30 +0200

    Added a new constant test

---
+++ src/main/java/dk/camelot64/kickc/test/ref/constantmin.asm
@@ -0,0 +1,20 @@
+  .const SCREEN = $400
+  .const STAR = $51
+  .const VIC = $d000
+  .const RED = $2
+  .const BGCOL = (VIC+($10*$2))+$1
+  jsr main
+main: {
+    lda #STAR
+    sta SCREEN
+    lda #RED
+    sta BGCOL
+    ldx #$28
+  b1:
+    lda #STAR+$1
+    sta SCREEN,x
+    inx
+    cpx #$50
+    bne b1
+    rts
+}

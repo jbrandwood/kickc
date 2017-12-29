5ec1070b41f4153cf299d11b529701cb17d54375
jespergravgaard <jesper@balmangravgaard.dk>
2017-12-29 11:40:03 +0100

    Improved tests for CI

--- src/main/java/dk/camelot64/kickc/Compiler.java
+++ src/main/java/dk/camelot64/kickc/Compiler.java
@@ -57,7 +57,7 @@ public class Compiler {
          }
          final CharStream fileStream = CharStreams.fromPath(file.toPath());
          imported.add(file.getAbsolutePath());
-         program.getLog().append("PARSING "+file.getAbsolutePath());
+         program.getLog().append("PARSING "+file.getPath());
          program.getLog().append(fileStream.toString());
          KickCLexer lexer = new KickCLexer(fileStream);
          KickCParser parser = new KickCParser(new CommonTokenStream(lexer));

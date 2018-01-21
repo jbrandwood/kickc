1d54fec7beb4609056aa334f0598f2f5255c8ccf
Jesper Gravgaard <jg@ramboll.com>
2018-01-21 11:39:06 +0100

    Put tests into suite to get deterministic sequencing.

---
+++ src/test/java/dk/camelot64/kickc/test/Tests.java
@@ -0,0 +1,13 @@
+package dk.camelot64.kickc.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+      TestFragments.class,
+      TestPrograms.class
+})
+
+public class Tests {
+}

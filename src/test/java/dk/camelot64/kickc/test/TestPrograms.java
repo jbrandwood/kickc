981d925a4d42aaac4be9e3f2b9f68d4e836d4610
Jesper Gravgaard <jg@ramboll.com>
2018-03-22 18:34:04 +0100

    Coding CHARGEN tester

--- src/test/java/dk/camelot64/kickc/test/TestPrograms.java
+++ src/test/java/dk/camelot64/kickc/test/TestPrograms.java
@@ -46,6 +46,11 @@ public class TestPrograms {
    }
 
    @Test
+   public void testChargenAnalysis() throws IOException, URISyntaxException {
+      compileAndCompare("chargen-analysis");
+   }
+
+   @Test
    public void testKeyboardSpace() throws IOException, URISyntaxException {
       compileAndCompare("test-keyboard-space");
    }

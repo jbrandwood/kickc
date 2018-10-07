287ffb46edc4935dc946868134ce92f2d6138cb3
Jesper Gravgaard <jg@ramboll.com>
2018-10-07 03:10:48 +0200

    Some test work

--- src/test/java/dk/camelot64/kickc/test/TestFragments.java
+++ src/test/java/dk/camelot64/kickc/test/TestFragments.java
@@ -18,6 +18,7 @@ import static junit.framework.TestCase.fail;
 /** Test the ASM fragment sub-system by loading/synthesizing a lot of different fragments and comparing with reference fragments. */
 public class TestFragments {
 
+   /*
    @BeforeClass
    public static void setUp() {
       AsmFragmentTemplateSynthesizer.initialize("src/main/fragment/");
@@ -277,6 +278,8 @@ public class TestFragments {
       }
       return false;
    }
+
+   */
 
    /** A signature that is part of a fragment signature. The signature may have sub-values (eg. if. it is a _derefidx_ signature */
    public static class Value {

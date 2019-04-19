4e93f0b056440f1bbcb08ddd3f26d558d3b0a75a
jespergravgaard <jesper@balmangravgaard.dk>
2019-04-19 11:25:29 +0200

    Fixing more tests with word arrays/pointers. Preparing for #139

--- src/test/ref/wfragment1.asm
+++ src/test/ref/wfragment1.asm
@@ -14,13 +14,14 @@ main: {
 }
 // move_enemy(byte register(Y) obj_slot)
 move_enemy: {
-    sec
-    lda OBJ_WORLD_X,y
-    sbc #1
-    sta OBJ_WORLD_X,y
-    lda OBJ_WORLD_X+1,y
-    sbc #0
-    sta OBJ_WORLD_X+1,y
+    tya
+    asl
+    tax
+    lda OBJ_WORLD_X,x
+    bne !+
+    dec OBJ_WORLD_X+1,x
+  !:
+    dec OBJ_WORLD_X,x
     rts
 }
   OBJ_WORLD_X: .fill 2*MAX_OBJECTS, 0

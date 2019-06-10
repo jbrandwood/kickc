3d8443d62bb28dd98282cfa40e2af198408f758d
jespergravgaard <jesper@balmangravgaard.dk>
2019-06-10 12:06:09 +0200

    Added test for length og struct array

--- src/test/ref/sizeof-struct.asm
+++ src/test/ref/sizeof-struct.asm
@@ -6,7 +6,7 @@
   .const SIZEOF_STRUCT_CIRCLE = 3
   .label SCREEN = $400
 main: {
-    // Struct Array
+    // Struct Arrays
     .const NUM_POINTS = 4
     .const NUM_CIRCLES = NUM_POINTS-1
     // Struct Types
@@ -20,7 +20,11 @@ main: {
     sta SCREEN+4
     lda #'0'+NUM_POINTS*SIZEOF_STRUCT_POINT
     sta SCREEN+6
-    lda #'0'+NUM_CIRCLES*SIZEOF_STRUCT_POINT
+    lda #'0'+NUM_POINTS*SIZEOF_STRUCT_POINT/SIZEOF_STRUCT_POINT
     sta SCREEN+7
+    lda #'0'+NUM_CIRCLES*SIZEOF_STRUCT_CIRCLE
+    sta SCREEN+8
+    lda #'0'+NUM_CIRCLES*SIZEOF_STRUCT_CIRCLE/SIZEOF_STRUCT_CIRCLE
+    sta SCREEN+9
     rts
 }

df4294414aeb929c0ffbfa7ecb4a0d5e402ce93e
jespergravgaard <jesper@balmangravgaard.dk>
2019-06-24 19:48:18 +0200

    Changed default to byte*

--- src/test/ref/pointer-void-3.asm
+++ src/test/ref/pointer-void-3.asm
@@ -5,27 +5,40 @@
   .label SCREEN = $400
   .label heap_head = 2
 main: {
+    .label buf1 = 4
+    .label buf2 = 6
     lda #<$c000
     sta heap_head
     lda #>$c000
     sta heap_head+1
     jsr malloc
+    lda malloc.return_2
+    sta malloc.return
+    lda malloc.return_2+1
+    sta malloc.return+1
     jsr malloc
     lda #'a'
     ldy #0
-    sta (heap_head),y
+    sta (buf1),y
     lda #'b'
-    sta (heap_head),y
-    lda (heap_head),y
+    sta (buf2),y
+    lda (buf1),y
     sta SCREEN
-    lda (heap_head),y
+    lda (buf2),y
     sta SCREEN+1
     rts
 }
 malloc: {
+    .label return = 4
+    .label return_1 = 6
+    .label return_2 = 6
     inc heap_head
     bne !+
     inc heap_head+1
   !:
+    lda heap_head
+    sta return_2
+    lda heap_head+1
+    sta return_2+1
     rts
 }

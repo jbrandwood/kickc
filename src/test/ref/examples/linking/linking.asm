6b0ef4408c7a42ca2afff8c369a855fb66ce95ca
Jesper Gravgaard <jg@ramboll.com>
2019-08-09 17:50:02 +0200

    Added test data.

---
+++ src/test/ref/examples/linking/linking.asm
@@ -0,0 +1,19 @@
+// Example showing how to perform linking using a linker-file
+// The linker file is created using KickAssembler segments.
+// See the KickAssembler manual for description of the format http://theweb.dk/KickAssembler/
+// Specifying the linker script file is done using the #pragma link(<file>)
+// It can also be specified using kickc command line option -T <file>
+  .file [name="linking.bin", type="bin", segments="XMega65Bin"]
+.segmentdef XMega65Bin [segments="Syscall, Code, Data, Stack, Zeropage"]
+.segmentdef Syscall [start=$8000, max=$81ff]
+.segmentdef Code [start=$8200, min=$8200, max=$bdff]
+.segmentdef Data [startAfter="Code", min=$8200, max=$bdff]
+.segmentdef Stack [min=$be00, max=$beff, fill]
+.segmentdef Zeropage [min=$bf00, max=$bfff, fill]
+  .label BGCOL = $d021
+.segment Code
+main: {
+  b1:
+    inc BGCOL
+    jmp b1
+}

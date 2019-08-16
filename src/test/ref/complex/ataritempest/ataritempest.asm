891a4c21c2e5d1a5f1410342797e7786218b23ed
jespergravgaard <jesper@balmangravgaard.dk>
2019-08-16 22:28:37 +0200

    Improved Atari Tempest Example Code.

--- src/test/ref/complex/ataritempest/ataritempest.asm
+++ src/test/ref/complex/ataritempest/ataritempest.asm
@@ -2,10 +2,13 @@
 // Each function of the kernal is a no-args function
 // The functions are placed in the SYSCALLS table surrounded by JMP and NOP
   .file [name="ataritempest.bin", type="bin", segments="AtariTempest"]
-.segmentdef AtariTempest [segments="Code, Data, Vectors"]
-.segmentdef Code [start=$4000, min=$4000, max=$bff9]
-.segmentdef Data [startAfter="Code"]
-.segmentdef Vectors [start=$bffa, min=$bffa, max=$bfff]
+.segmentdef AtariTempest [segments="Code, RomData, Vectors"]
+.segmentdef Code [start=$9000, min=$9000, max=$Fff9]
+.segmentdef RomData [startAfter="Code", min=$9000, max=$Fff9]
+.segmentdef RamData [start=$200, min=$200, max=$7ff]
+.segmentdef Vectors [start=$FFFA, min=$FFFA, max=$FFFF, fill]
+.segment Code
+init:
   .label BGCOL = $c01a
 .segment Code
 main: {
@@ -26,8 +29,19 @@ nmiHandler: {
     rti
 }
 entryPoint: {
-    inc BGCOL
+    ldx #0
+  b1:
+    lda MESSAGE,x
+    sta SCREEN,x
+    inx
+    cpx #$32
+    bne b1
     rts
 }
+.segment RomData
+  MESSAGE: .text "hello world"
+  .byte 0
+.segment RamData
+  SCREEN: .fill $32, 0
 .segment Vectors
   VECTORS: .word nmiHandler, entryPoint

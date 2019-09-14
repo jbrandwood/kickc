bc1bb7d2e53987072e6af949a041b3f752fa8852
jespergravgaard <jesper@balmangravgaard.dk>
2019-09-14 08:09:34 +0200

    Fixed test

--- src/test/ref/struct-ptr-34.asm
+++ src/test/ref/struct-ptr-34.asm
@@ -1,6 +1,4 @@
 // Example of a struct containing an array
-// It works on the surface - but illustrates the problem with structs containing arrays treating them like pointers.
-// https://gitlab.com/camelot/kickc/issues/312
 .pc = $801 "Basic"
 :BasicUpstart(main)
 .pc = $80d "Program"

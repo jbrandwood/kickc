6862698fb7e9fc8a63d5e2eb146f73c5d0fa2052
jespergravgaard <jesper@balmangravgaard.dk>
2020-02-23 19:26:53 +0100

    Eliminated redundant methods got getting assignments/usages in Pass2SsaOptimization.

--- src/main/java/dk/camelot64/kickc/passes/Pass2ConditionalAndOrRewriting.java
+++ src/main/java/dk/camelot64/kickc/passes/Pass2ConditionalAndOrRewriting.java
@@ -3,6 +3,7 @@ package dk.camelot64.kickc.passes;
 import dk.camelot64.kickc.model.Comment;
 import dk.camelot64.kickc.model.ControlFlowBlock;
 import dk.camelot64.kickc.model.Program;
+import dk.camelot64.kickc.model.VariableReferenceInfos;
 import dk.camelot64.kickc.model.operators.Operators;
 import dk.camelot64.kickc.model.statements.Statement;
 import dk.camelot64.kickc.model.statements.StatementAssignment;
@@ -10,9 +11,14 @@ import dk.camelot64.kickc.model.statements.StatementConditionalJump;
 import dk.camelot64.kickc.model.statements.StatementPhiBlock;
 import dk.camelot64.kickc.model.symbols.Label;
 import dk.camelot64.kickc.model.symbols.Scope;
-import dk.camelot64.kickc.model.values.*;
+import dk.camelot64.kickc.model.values.LabelRef;
+import dk.camelot64.kickc.model.values.RValue;
+import dk.camelot64.kickc.model.values.ScopeRef;
+import dk.camelot64.kickc.model.values.VariableRef;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 
 /**
  * Compiler Pass rewriting conditional jumps that use && or || operators
@@ -25,12 +31,10 @@ public class Pass2ConditionalAndOrRewriting extends Pass2SsaOptimization {
 
    @Override
    public boolean step() {
-      Map<LValue, StatementAssignment> assignments = getAllAssignments();
-      Map<RValue, List<Statement>> usages = getAllUsages();
       boolean done = false;
       boolean modified = false;
       while(!done) {
-         VariableRef obsoleteConditionVar = findAndRewriteBooleanCondition(assignments, usages);
+         VariableRef obsoleteConditionVar = findAndRewriteBooleanCondition();
          if(obsoleteConditionVar != null) {
             Collection<VariableRef> obsoleteVars = new ArrayList<>();
             obsoleteVars.add(obsoleteConditionVar);
@@ -47,31 +51,40 @@ public class Pass2ConditionalAndOrRewriting extends Pass2SsaOptimization {
    /**
     * Look through the entire program looking for an if() condition that uses &&, || or !.
     * When found rewrite it (adding blocks)
+    *
     * @return Null if no condition was found to rewrite. The now obsolete variable containing the && / || / ! to be removed.
     */
-   private VariableRef  findAndRewriteBooleanCondition(Map<LValue, StatementAssignment> assignments, Map<RValue, List<Statement>> usages) {
+   private VariableRef findAndRewriteBooleanCondition() {
+      final VariableReferenceInfos variableReferenceInfos = getProgram().getVariableReferenceInfos();
       for(ControlFlowBlock block : getGraph().getAllBlocks()) {
          for(Statement statement : block.getStatements()) {
             if(statement instanceof StatementConditionalJump) {
                StatementConditionalJump conditional = (StatementConditionalJump) statement;
-               if(conditional.getrValue1()==null && conditional.getOperator()==null) {
+               if(conditional.getrValue1() == null && conditional.getOperator() == null) {
                   RValue conditionRValue = conditional.getrValue2();
-                  if(conditionRValue instanceof VariableRef && usages.get(conditionRValue).size() == 1) {
-                     VariableRef conditionVar = (VariableRef) conditionRValue;
-                     StatementAssignment conditionAssignment = assignments.get(conditionVar);
-                     if(conditionAssignment!=null) {
-                        if(Operators.LOGIC_AND.equals(conditionAssignment.getOperator())) {
-                           // Found if() with logical && condition - rewrite to if(c1) if(c2) { xx }
-                           rewriteLogicAnd(block, conditional, conditionAssignment);
-                           return conditionVar;
-                        } else if(Operators.LOGIC_OR.equals(conditionAssignment.getOperator())) {
-                           // Found if() with logical || condition - rewrite to if(c1) goto x else if(c2) goto x else goto end, x:{ xx } end:
-                           rewriteLogicOr(block, conditional, conditionAssignment);
-                           return conditionVar;
-                        } else if(Operators.LOGIC_NOT.equals(conditionAssignment.getOperator())) {
-                           // Found if() with logical ! condition - rewrite to if(!c1) goto x else goto end, x:{ xx } end:
-                           rewriteLogicNot(block, conditional, conditionAssignment);
-                           return conditionVar;
+                  if(conditionRValue instanceof VariableRef) {
+                     final int conditionRValueUsages = variableReferenceInfos.getVarUseStatements((VariableRef) conditionRValue).size();
+                     if(conditionRValueUsages == 1) {
+                        VariableRef conditionVar = (VariableRef) conditionRValue;
+                        final Integer conditionDefineStatementIdx = variableReferenceInfos.getVarDefineStatement(conditionVar);
+                        if(conditionDefineStatementIdx != null) {
+                           final Statement conditionDefineStatement = getGraph().getStatementByIndex(conditionDefineStatementIdx);
+                           if(conditionDefineStatement instanceof StatementAssignment) {
+                              StatementAssignment conditionAssignment = (StatementAssignment) conditionDefineStatement;
+                              if(Operators.LOGIC_AND.equals(conditionAssignment.getOperator())) {
+                                 // Found if() with logical && condition - rewrite to if(c1) if(c2) { xx }
+                                 rewriteLogicAnd(block, conditional, conditionAssignment);
+                                 return conditionVar;
+                              } else if(Operators.LOGIC_OR.equals(conditionAssignment.getOperator())) {
+                                 // Found if() with logical || condition - rewrite to if(c1) goto x else if(c2) goto x else goto end, x:{ xx } end:
+                                 rewriteLogicOr(block, conditional, conditionAssignment);
+                                 return conditionVar;
+                              } else if(Operators.LOGIC_NOT.equals(conditionAssignment.getOperator())) {
+                                 // Found if() with logical ! condition - rewrite to if(!c1) goto x else goto end, x:{ xx } end:
+                                 rewriteLogicNot(block, conditional, conditionAssignment);
+                                 return conditionVar;
+                              }
+                           }
                         }
                      }
                   }
@@ -84,13 +97,14 @@ public class Pass2ConditionalAndOrRewriting extends Pass2SsaOptimization {
 
    /**
     * Rewrite logical && condition if(c1&&c2) { xx } to if(c1) if(c2) { xx }
+    *
     * @param block The block containing the current if()
     * @param conditional The if()-statement
     * @param conditionAssignment The assignment defining the condition variable.
     */
    private void rewriteLogicAnd(ControlFlowBlock block, StatementConditionalJump conditional, StatementAssignment conditionAssignment) {
       // Found an if with a logical && condition - rewrite to if(c1) if(c2) { xx }
-      getLog().append("Rewriting && if()-condition to two if()s "+conditionAssignment.toString(getProgram(), false));
+      getLog().append("Rewriting && if()-condition to two if()s " + conditionAssignment.toString(getProgram(), false));
       ScopeRef currentScopeRef = block.getScope();
       Scope currentScope = getScope().getScope(currentScopeRef);
       // Add a new block containing the second part of the && condition expression
@@ -118,12 +132,13 @@ public class Pass2ConditionalAndOrRewriting extends Pass2SsaOptimization {
 
    /**
     * Rewrite logical || condition if(c1||c2) { xx } to if(c1) goto x else if(c2) goto x else goto end, x:{ xx } end:
+    *
     * @param block The block containing the current if()
     * @param conditional The if()-statement
     * @param conditionAssignment The assignment defining the condition variable.
     */
    private void rewriteLogicOr(ControlFlowBlock block, StatementConditionalJump conditional, StatementAssignment conditionAssignment) {
-      getLog().append("Rewriting || if()-condition to two if()s "+conditionAssignment.toString(getProgram(), false));
+      getLog().append("Rewriting || if()-condition to two if()s " + conditionAssignment.toString(getProgram(), false));
       ScopeRef currentScopeRef = block.getScope();
       Scope currentScope = getScope().getScope(currentScopeRef);
       // Add a new block containing the second part of the && condition expression
@@ -174,12 +189,13 @@ public class Pass2ConditionalAndOrRewriting extends Pass2SsaOptimization {
 
    /**
     * Rewrite logical ! condition if(!c1) { xx } to if(c1) goto end else goto x, x:{ xx } end:
+    *
     * @param block The block containing the current if()
     * @param conditional The if()-statement
     * @param conditionAssignment The assignment defining the condition variable.
     */
    private void rewriteLogicNot(ControlFlowBlock block, StatementConditionalJump conditional, StatementAssignment conditionAssignment) {
-      getLog().append("Rewriting ! if()-condition to reversed if() "+conditionAssignment.toString(getProgram(), false));
+      getLog().append("Rewriting ! if()-condition to reversed if() " + conditionAssignment.toString(getProgram(), false));
       // Rewrite the conditional to use only the first part of the && condition expression
       LabelRef defaultSuccessor = block.getDefaultSuccessor();
       LabelRef conditionalSuccessor = block.getConditionalSuccessor();
--- src/main/java/dk/camelot64/kickc/passes/Pass2ConditionalJumpSimplification.java
+++ src/main/java/dk/camelot64/kickc/passes/Pass2ConditionalJumpSimplification.java
@@ -7,13 +7,14 @@ import dk.camelot64.kickc.model.statements.Statement;
 import dk.camelot64.kickc.model.statements.StatementAssignment;
 import dk.camelot64.kickc.model.statements.StatementConditionalJump;
 import dk.camelot64.kickc.model.statements.StatementInfos;
+import dk.camelot64.kickc.model.symbols.Variable;
 import dk.camelot64.kickc.model.values.RValue;
 import dk.camelot64.kickc.model.values.VariableRef;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Compiler Pass simplifying conditional jumps that are simple comparisons
@@ -26,14 +27,13 @@ public class Pass2ConditionalJumpSimplification extends Pass2SsaOptimization {
 
    @Override
    public boolean step() {
-      final Map<RValue, List<Statement>> usages = getAllUsages();
-      final List<VariableRef> simpleConditionVars = getSimpleConditions(usages);
+      final List<VariableRef> simpleConditionVars = getSimpleConditions();
       removeAssignments(getGraph(), simpleConditionVars);
       deleteSymbols(getScope(), simpleConditionVars);
       return (simpleConditionVars.size() > 0);
    }
 
-   private List<VariableRef> getSimpleConditions(final Map<RValue, List<Statement>> usages) {
+   private List<VariableRef> getSimpleConditions() {
       final VariableReferenceInfos variableReferenceInfos = getProgram().getVariableReferenceInfos();
       final List<VariableRef> simpleConditionVars = new ArrayList<>();
       for(ControlFlowBlock block : getGraph().getAllBlocks()) {
@@ -42,10 +42,11 @@ public class Pass2ConditionalJumpSimplification extends Pass2SsaOptimization {
                StatementConditionalJump conditionalJump = (StatementConditionalJump) statement;
                if(conditionalJump.getrValue1() == null && conditionalJump.getOperator() == null) {
                   RValue conditionRValue = conditionalJump.getrValue2();
-                  if(conditionRValue instanceof VariableRef && usages.get(conditionRValue).size() == 1) {
+                  if(conditionRValue instanceof VariableRef) {
                      VariableRef conditionVar = (VariableRef) conditionRValue;
+                     final Collection<Integer> conditionRvalueUsages = variableReferenceInfos.getVarUseStatements(conditionVar);
                      final Integer conditionDefineStmtIdx = variableReferenceInfos.getVarDefineStatement(conditionVar);
-                     if(conditionDefineStmtIdx != null) {
+                     if(conditionRvalueUsages.size() == 1 && conditionDefineStmtIdx != null) {
                         final Statement conditionDefineStmt = getGraph().getStatementByIndex(conditionDefineStmtIdx);
                         if(conditionDefineStmt instanceof StatementAssignment && ((StatementAssignment) conditionDefineStmt).getOperator() != null) {
                            StatementAssignment conditionAssignment = (StatementAssignment) conditionDefineStmt;
@@ -59,7 +60,7 @@ public class Pass2ConditionalJumpSimplification extends Pass2SsaOptimization {
                               case "=<":
                               case ">=":
                               case "=>":
-                                 final Collection<VariableRef> referencedLoadStoreVariables = getReferencedLoadStoreVariables(conditionAssignment.getrValue1());
+                                 final Collection<Variable> referencedLoadStoreVariables = getReferencedLoadStoreVariables(conditionAssignment.getrValue1());
                                  referencedLoadStoreVariables.addAll(getReferencedLoadStoreVariables(conditionAssignment.getrValue2()));
                                  boolean isSimple = true;
                                  if(referencedLoadStoreVariables.size() > 0) {
@@ -68,8 +69,8 @@ public class Pass2ConditionalJumpSimplification extends Pass2SsaOptimization {
                                     final StatementInfos statementInfos = getProgram().getStatementInfos();
                                     Collection<Statement> statementsBetween = getGraph().getStatementsBetween(conditionAssignment, conditionalJump, statementInfos);
                                     for(Statement statementBetween : statementsBetween) {
-                                       for(VariableRef referencedLoadStoreVariable : referencedLoadStoreVariables) {
-                                          if(variableReferenceInfos.getDefinedVars(statementBetween).contains(referencedLoadStoreVariable)) {
+                                       for(Variable referencedLoadStoreVariable : referencedLoadStoreVariables) {
+                                          if(variableReferenceInfos.getDefinedVars(statementBetween).contains(referencedLoadStoreVariable.getVariableRef())) {
                                              // A referenced load/store-variable is modified in a statement between the assignment and the condition!
                                              isSimple = false;
                                              getLog().append("Condition not simple " + conditionVar.toString(getProgram()) + " " + conditionalJump.toString(getProgram(), false));
@@ -106,14 +107,12 @@ public class Pass2ConditionalJumpSimplification extends Pass2SsaOptimization {
     * @param rValue The RValue
     * @return All referenced load/store variables
     */
-   private Collection<VariableRef> getReferencedLoadStoreVariables(RValue rValue) {
-      List<VariableRef> referencedLoadStoreVariables = new ArrayList<>();
-      final Collection<VariableRef> vars1 = VariableReferenceInfos.getReferencedVars(rValue);
-      for(VariableRef variableRef : vars1) {
-         if(getScope().getVariable(variableRef).isKindLoadStore())
-            referencedLoadStoreVariables.add(variableRef);
-      }
-      return referencedLoadStoreVariables;
+   private Collection<Variable> getReferencedLoadStoreVariables(RValue rValue) {
+      return VariableReferenceInfos.getReferencedVars(rValue)
+            .stream()
+            .map(variableRef -> getScope().getVariable(variableRef))
+            .filter(Variable::isKindLoadStore)
+            .collect(Collectors.toList());
    }
 
 }

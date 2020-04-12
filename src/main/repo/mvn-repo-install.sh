b4787eb45c819c587acc6f1cfed5441236b430b1
jespergravgaard <jesper@balmangravgaard.dk>
2020-04-12 08:03:01 +0200

    Updated to KickAss 5.13. Closes #389

--- src/main/repo/mvn-repo-install.sh
+++ src/main/repo/mvn-repo-install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.9
+mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.13

1b9f523f8f2a0373852e7af66617277b70bd409d
jespergravgaard <jesper@balmangravgaard.dk>
2020-05-16 09:39:24 +0200

    Fixed processor port addresses.

--- src/main/kc/include/mos7501.h
+++ src/main/kc/include/mos7501.h
@@ -4,6 +4,8 @@
 // The processor port of the MOS 7501 CPU
 // http://hackjunk.com/2017/06/23/commodore-16-plus-4-8501-to-6510-cpu-conversion/
 struct MOS7501_PORT {
+    // The data direction of the port
+    char DDR;
     // The on-chip processor port
     //   P0: SER DAT OUT
     //   P1: SER CLK OUT / CASS WRT
@@ -13,6 +15,4 @@ struct MOS7501_PORT {
     //   P6: SER CLK IN
     //   P7: SER DAT IN
     char PORT;
-    // The data direction of the port
-    char DDR;
 };

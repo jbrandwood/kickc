be85d43f1c5e3ef845c42022447cf3dbc07dbd3d
jespergravgaard <jesper@balmangravgaard.dk>
2020-07-26 00:57:17 +0200

    Upgraded to Kickassembler 5.16

--- src/main/repo/mvn-repo-install.sh
+++ src/main/repo/mvn-repo-install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.15
+mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.16

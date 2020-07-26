afae4975f14df05eee9f3604f7cf5417ba95f478
jespergravgaard <jesper@balmangravgaard.dk>
2020-07-26 02:01:02 +0200

    Updated to KickAssembler 5.16-65CE02 (my own hacked version).

--- src/main/repo/mvn-repo-install.sh
+++ src/main/repo/mvn-repo-install.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.16
+# mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.16
+mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Users/jespergravgaard/c64/kickassembler65ce02/out/KickAss65CE02.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.16-65ce02.b

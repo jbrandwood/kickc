c61ec7370d139b12ae47087828e108082af192fe
jespergravgaard <jesper@balmangravgaard.dk>
2020-09-23 08:32:29 +0200

    Started MEGA65 DMA test.

---
+++ src/test/kc/examples/mega65/dma-test.c
@@ -0,0 +1,8 @@
+// Tests the MEGA65 DMA
+
+// Appendix J in https://mega.scryptos.com/sharefolder-link/MEGA/MEGA65+filehost/Docs/MEGA65-Book_draft.pdf
+// C65 Manual http://www.zimmers.net/cbmpics/cbm/c65/c65manual.txt
+// DMA lists https://raw.githubusercontent.com/MEGA65/c65-specifications/master/c65manualupdated.txt
+// DMA lists https://c65gs.blogspot.com/2019/03/auto-detecting-required-revision-of.html
+// DMA list options https://c65gs.blogspot.com/2018/01/improving-dmagic-controller-interface.html
+// DMAgic VHDL source https://github.com/MEGA65/mega65-core/blob/master/src/vhdl/gs4510.vhdl#L4364

205ce4eb6c5fe08f9b263f40e73717481146b1d2
Jesper Gravgaard <jg@ramboll.com>
2020-12-14 13:33:10 +0100

    Added 6522 VIA#1 and VIA#1 to CX16. #581

--- src/main/kc/include/cx16.h
+++ src/main/kc/include/cx16.h
@@ -3,6 +3,27 @@
 // https://github.com/commanderx16/x16-docs/blob/master/Commander%20X16%20Programmer's%20Reference%20Guide.md
 
 #include <cx16-vera.h>
+#include <mos6522.h>
+
+// The VIA#1: ROM/RAM Bank Control
+// Port A Bits 0-7 RAM bank
+// Port B Bits 0-2 ROM bank
+// Port B Bits 3-7 [TBD]
+struct MOS6522_VIA * const VIA1 = 0x9160;
+// The VIA#2: Keyboard/Joy/Mouse
+// Port A Bit  0   KBD PS/2 DAT
+// Port A Bit  1   KBD PS/2 CLK
+// Port A Bit  2   [TBD]
+// Port A Bit  3   JOY1/2 LATCH
+// Port A Bit  4   JOY1 DATA
+// Port A Bit  5   JOY1/2 CLK
+// Port A Bit  6   JOY2 DATA
+// Port A Bit  7   [TBD]
+// Port B Bit  0   MOUSE PS/2 DAT
+// Port B Bit  1   MOUSE PS/2 CLK
+// Port B Bits 2-7 [TBD]
+// NOTE: The pin assignment of the NES/SNES controller is likely to change.
+struct MOS6522_VIA * const VIA2 = 0x9070;
 
 // Interrupt Vectors
 // https://github.com/commanderx16/x16-emulator/wiki/(ASM-Programming)-Interrupts-and-interrupt-handling

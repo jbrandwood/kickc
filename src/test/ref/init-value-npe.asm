7e6c97779b62175321d3d00fa6546db636b1150f
Jesper Gravgaard <jg@ramboll.com>
2021-04-21 19:34:18 +0200

    Fixed problem with NPE when removing unused code can leave global constants referencing removed variables. Closes #650

---
+++ src/test/ref/init-value-npe.asm
@@ -0,0 +1,26 @@
+// https://gitlab.com/camelot/kickc/-/issues/650
+  // Commodore 64 PRG executable file
+.file [name="init-value-npe.prg", type="prg", segments="Program"]
+.segmentdef Program [segments="Basic, Code, Data"]
+.segmentdef Basic [start=$0801]
+.segmentdef Code [start=$80d]
+.segmentdef Data [startAfter="Code"]
+.segment Basic
+:BasicUpstart(main)
+  .label myscreen = $7000
+.segment Code
+main: {
+    // memset(myscreen)
+    jsr memset
+    // }
+    rts
+}
+memset: {
+    .label str = myscreen
+    .label dst = str
+    // *dst = 0
+    lda #0
+    sta dst
+    // }
+    rts
+}

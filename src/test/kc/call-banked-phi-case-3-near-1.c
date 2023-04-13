70012690ddbf1842a4a4241565025db2cdee602c
Flight_Control <sven.van.de.velde@telenet.be>
2023-04-13 13:07:30 +0200

    Revert "- Resolved issue of nobank without parameters. Now it works ... The CPreprocessor adds () and the KickCParser.g4 demands brackets as part of the syntax. But you can write without in the code. - Retested all test cases."
    This reverts commit 82a6c56e

--- src/test/kc/call-banked-phi-case-3-near-1.c
+++ src/test/kc/call-banked-phi-case-3-near-1.c
@@ -30,6 +30,10 @@
 
 char* const SCREEN = (char*)0x0400;
 
+#pragma code_seg(Code)
+void main(void) {
+    SCREEN[0] = plus('0', 7); // close call
+}
 
 #pragma code_seg(RAM_Bank1)
 #pragma bank(cx16_ram, 1)
@@ -43,8 +47,4 @@ char min(char a, char b) {
     return a+b;
 }
 
-#pragma code_seg(Code)
-void main(void) {
-    SCREEN[0] = plus('0', 7); // close call
-}
 

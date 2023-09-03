cec787e91b681dd4d5470ff179338901c038feb0
jespergravgaard <jesper@balmangravgaard.dk>
2023-09-03 14:44:43 +0200

    Upgraded to KickAss 5.25

--- src/main/repo/mvn-repo-install-kickass.sh
+++ src/main/repo/mvn-repo-install-kickass.sh
@@ -4,7 +4,7 @@
 cp ./repo/cml/kickass/kickassembler/maven-metadata.xml ./repo/cml/kickass/kickassembler/maven-metadata-local.xml
 
 # mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Applications/KickAssembler/KickAss.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.16
-mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Users/jespergravgaard/c64/kickassembler65ce02/out/KickAss65CE02.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.24-65ce02.e
+mvn install:install-file -Dmaven.repo.local=./repo/ -Dfile=/Users/jespergravgaard/c64/kickassembler65ce02/out/KickAss65CE02.jar -DgroupId=cml.kickass -DartifactId=kickassembler -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true -Dversion=5.25-65ce02
 
 # Finalize by making the local metadata official
 pushd ./repo/cml/kickass/kickassembler
@@ -12,7 +12,7 @@ mv maven-metadata-local.xml maven-metadata.xml
 mv maven-metadata-local.xml.md5 maven-metadata.xml.md5
 mv maven-metadata-local.xml.sha1 maven-metadata.xml.sha1
 popd
-
+]
 # Remove stuff that Maven adds, that we don't need
 rm -rf ./repo/classworlds
 rm -rf ./repo/junit
